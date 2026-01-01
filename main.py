98d50c79d795445adfb07c4a3bbc6171b4daed0e
Abderrahman YOUABD <youabd50@gmail.com>
2026-01-01 03:02:07 +0100

    feat(cli): add command-line interface for Gemini generation with environment configuration

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+import json
+import argparse
+
+
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+model_name = os.getenv("GEMINI_MODEL")
+
+
 if __name__ == "__main__":
-    print("Stuff works")
-
-
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--prompt", "-p", type=str, required=True)
+    args = parser.parse_args()
+    response = client.models.generate_content(model=model_name, contents=args.prompt)
+    print(response.text)
