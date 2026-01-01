63eda45c23074713f49cd0b67998fca7ef716e2a
Abderrahman YOUABD <youabd50@gmail.com>
2026-01-01 03:16:04 +0100

    feat(main): add verbose flag, error handling, and switch to typed content generation

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 import json
 import argparse
-
+import sys
 
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
@@ -13,6 +14,18 @@ model_name = os.getenv("GEMINI_MODEL")
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--prompt", "-p", type=str, required=True)
+    parser.add_argument("--verbose", "-v", action="store_true", default=False)
     args = parser.parse_args()
-    response = client.models.generate_content(model=model_name, contents=args.prompt)
-    print(response.text)
+    try:
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=args.prompt)])
+        ]
+        response = client.models.generate_content(model=model_name, contents=messages)
+        if args.verbose:
+            print(response)
+        else:
+            print(response.text)
+    except Exception as e:
+        print(e)
+    finally:
+        sys.exit(0)
